a095a879d6e112463ef9e62adf4fc18467137c2d
jonahmacrae <jonahmacrae@gmail.com>
2025-12-03 10:28:28 +0000

    part 1 complete

--- day-1/secret-entrance.cpp
+++ day-1/secret-entrance.cpp
@@ -2,18 +2,45 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <utility>
 
 int main(){
     std::cout << "Welcome to the secret entrance!" << std::endl;
 
     std::ifstream file("input.txt");
-    std::vector<std::string> moves;
+    std::vector<std::pair<char, int>> moves;
     std::string move;
     
     while (file >> move) {
-        moves.push_back(move);
+        char direction = move[0];               // L or R
+        int steps = std::stoi(move.substr(1));  // number of steps
+        moves.push_back({direction, steps});
     }
     file.close();
+
+    // To access direction, use moves[i].first
+    // To access steps, use moves[i].second
+
+    int dial = 50;
+    int pointAtZeroCount = 0;
+    
+    for (int i = 0; i < moves.size(); i++) {
+        if (moves[i].first == 'L') {
+            dial -= moves[i].second;
+            std::cout << "Dial: " << dial << std::endl;
+        } else {
+            dial += moves[i].second;
+            std::cout << "Dial: " << dial << std::endl;
+        }
+
+        dial %= 100;
+
+        if (dial == 0) {
+            pointAtZeroCount++;
+        }
+    }
+
+    std::cout << "The dial points at zero " << pointAtZeroCount << " times." << std::endl;
 
     return 0;
 }
